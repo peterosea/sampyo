6a41423e05fda9a341f88002105d3f819997ba8e
999hyeon <999@hyeon.pro>
2021-07-30 16:41:23 +0900

    :sparkles: initial roots/sage

---
+++ Theme/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    content: ['./app/**/*.php', './resources/**/*.{php,vue,js}'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
