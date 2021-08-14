1f53be650957eafbcb9edbf2b4a4f0bd797f7a2f
999hyeon <999@hyeon.pro>
2021-08-14 15:54:42 +0900

    :sparkles: 프레스
    - Archive
    - single
    - taxonomy
    
    Resolves: #40

---
+++ Theme/resources/views/taxonomy-press_category.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+  @include('partials.header-archive-newsroom')
+  @include('partials.nav-fixed')
+  <main>
+    <div class="container lg:max-w-screen-lg mx-auto">
+      @if (!have_posts())
+        <div class="alert alert-warning">
+          {{ __('Sorry, no results were found.', 'sage') }}
+        </div>
+        {!! get_search_form(false) !!}
+      @endif
+      <ul class="post-wrap">
+        @while (have_posts()) @php(the_post())
+          @include('partials.content-newsroom')
+        @endwhile
+      </ul>
+    </div>
+
+    @empty(!$pagination = get_the_posts_pagination( array(
+      'mid_size' => 2,
+      'prev_text' => __( '이전' ),
+      'next_text' => __( '다음' ),
+    )))
+      <div class="container lg:max-w-screen-lg mx-auto">
+        <div class="page-pagination-number">
+          {!! $pagination !!}
+        </div>
+      </div>
+    @endempty
+  </main>
+@endsection
--- Theme/wp-functions/archive.php
+++ Theme/wp-functions/archive.php
@@ -14,5 +14,23 @@ function wpd_testimonials_query($query)
       && $query->is_main_query()) {
         $query->set('posts_per_page', 5);
     }
+
+    if (! is_admin()
+      && $query->is_post_type_archive('press')
+      && $query->is_main_query()) {
+        $query->set('posts_per_page', 5);
+    }
+
+    if (! is_admin()
+      && $query->is_tax('press_category')
+      && $query->is_main_query()) {
+        $query->set('posts_per_page', 5);
+    }
+    
+    if (! is_admin()
+      && $query->is_tax('blog_category')
+      && $query->is_main_query()) {
+        $query->set('posts_per_page', 5);
+    }
 }
 add_action('pre_get_posts', 'wpd_testimonials_query');
