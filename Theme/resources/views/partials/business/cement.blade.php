3bc6991c584729b760432435004ed4442000a935
999hyeon <999@hyeon.pro>
2021-08-24 15:24:58 +0900

    :sparkles: 비즈니스 - 시멘트 페이지
    Markup, assets
    
    Resolves: #58

---
+++ Theme/resources/views/partials/business/cement.blade.php
@@ -0,0 +1,105 @@
+<div class="container lg:max-w-screen-lg mx-auto">
+  <div class="business__page-title mb-11">
+    세계 최고 수준의 시멘트 생산/공급 능력을 갖추고, <br/>
+    국내 시멘트 산업을 선도하고 있습니다.
+  </div>
+  <div class="flex -mx-6 mb-20">
+    <div class="w-1/2 px-6">
+      <div class="prose">
+        <p>
+          대한민국 최초의 시멘트 기업으로 출발한 삼표시멘트는 연간 1,100만톤의 시멘트 생산능력을 보유한 삼척공장(강원도 삼척시 소재)에서 시멘트를 생산하여, 전국 16개 출하기지를 통해 시멘트 및 2차 제품을 판매하고 있습니다.
+        </p>
+      </div>
+    </div>
+    <div class="w-1/2 px-6">
+      <div class="prose">
+        <p>
+          국내 최초의 현대적 시멘트 생산공장인 삼척공장은 가장 중요한 원재료인 석회석을 40년 이상 안정적으로 조달할 수 있는 공급망을 구축하였고, 생산공정에 필요한 유연탄을 공급받을 수 있는 항구가 근거리에 있는 최적의 입지에 자리하고 있습니다. 
+        </p>
+        <p>
+          삼표시멘트는 탁월한 물류 네트워크와 입지조건의 장점을 살려 국내외 고객의 니즈를 충족할 수 있는 고품질 제품을 제조, 판매할 수 있는 여건을 확보하였습니다.
+        </p>
+      </div>
+    </div>
+  </div>
+  <img
+    src="@php echo get_theme_file_uri('resources/images/dji-0232-sample.jpg') @endphp"
+    srcset="
+      @php echo get_theme_file_uri('resources/images/dji-0232-sample@2x.jpg') @endphp 2x,
+      @php echo get_theme_file_uri('resources/images/dji-0232-sample@3x.jpg') @endphp 3x"
+    alt=""
+  >
+  <div class="mb-32"></div>
+  <div class="coverbox-leftFull mb-28">
+    <div class="business__page-title mb-11">
+      사회적, 환경적 책임을 존중합니다.
+    </div>
+    <div class="prose mb-16">
+      <p>삼표시멘트는 제품과 서비스를 넘어 지속가능한 미래를 만들기 위하여 기업의 혁신문화를 촉진하고 있으며, 긍정적인 사회적, 환경적 성과를 달성하기 위해 노력을 아끼지 않습니다. 삼표시멘트는 1980년 에너지 파동 시 국내최초로 소성로의 연료를 벙커C유에서 유연탄으로 대체하였으며, 2004년에는 소성로의 폐열을 이용하여 전기를 생산하는 폐열발전소를 가동하였습니다.</p>
+    </div>
+    <div class="flex -mx-6">
+      <div class="w-1/2 px-6">
+        <img
+        src="@php echo get_theme_file_uri('resources/images/5387.jpg') @endphp"
+        srcset="
+          @php echo get_theme_file_uri('resources/images/5387@2x.jpg') @endphp 2x,
+          @php echo get_theme_file_uri('resources/images/5387@3x.jpg') @endphp 3x"
+        alt=""
+        >
+      </div>
+      <div class="w-1/2 px-6 flex items-center">
+        <div class="prose">
+          <p>이러한 경험을 바탕으로, ESG 경영 체제로의 전환을 위해 친환경 사업을 정관상 목적사업으로 추가하는 한편, 환경오염물 배출 저감을 위한 환경개선설비, 폐플라스틱, 폐비닐 등의 순환자원 활용을 통해 온실가스 배출량을 저감할 수 있는 순환자원처리시설 및 기존 폐열 발전설비 보강 등 친환경 사업에 관한 투자를 확충하고 있습니다.</p>
+          <p>삼표시멘트는 안전보건 경영방침을 제정하고 안전보건목표를 수립하여 쾌적하고 안전한 무재해 사업장을 실현하기 위해 잠재 위험의 주기적 평가, 전구성원의 자발적 참여를 위한 안전보건의식 교육 및 홍보활동을 지속적으로 수행하고 있습니다. </p>
+          <p>
+            또한 삼표시멘트는 지역에 기반을 둔 향토기업으로 지역사회와의 동반 성장과 발전을 위해 지역상생협의체 운영, 각종 지원사업 (지역발전기금, 교육사업, 지역주민지원, 지자체협력, 지역물품구입) 등을 수행하고 있습니다.<br/>
+            삼표시멘트는 환경투자, 안전보건, 사회 공헌활동을 확대해 나감으로써 지속가능한 경영을 실천하고 사회적 책임을 더욱 강화할 것입니다.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="business__page-title mb-11">
+    지속가능한 미래를 위한 기술 개발
+  </div>
+  <div class="flex -mx-6 mb-20">
+    <div class="w-1/2 px-6">
+      <div class="prose">
+        <p>
+          삼표시멘트 기술연구소는 시멘트 공정개선 및 생산기술 개발, 특수시멘트와 특수콘크리트 등의 차세대 건설재료 개발, 각종 고기능·고성능 신소재의 개발에 이르기까지 종합적인 연구개발 활동을 추진하여 시멘트 산업의 기술을 선도하였고, 향후 국내 건자재 분야의 선진화를 위해 신기술의 실용화에 박차를 가하고 있습니다.
+        </p>
+      </div>
+    </div>
+    <div class="w-1/2 px-6">
+      <div class="prose">
+        <p>
+          또한, 국내 최초로 소성(燒成)용 연료를 벙커C유에서 유연탄으로 교체하는데 성공하여 에너지 절약에 기여하였고, 환경과 사람을 우선으로 생각하는 환경친화 기업으로서 환경경영방침을 수립하여 시멘트 산업을 환경산업으로 전환하기 위해 현재 활발히 연구를 진행하고 있습니다.
+        </p>
+      </div>
+    </div>
+  </div>
+  <img
+    src="@php echo get_theme_file_uri('resources/images/tip-249-t-014163-1.jpg') @endphp"
+    srcset="
+      @php echo get_theme_file_uri('resources/images/tip-249-t-014163-1@2x.jpg') @endphp 2x,
+      @php echo get_theme_file_uri('resources/images/tip-249-t-014163-1@3x.jpg') @endphp 3x"
+    alt=""
+  >
+  <div class="mb-20"></div>
+  <div class="flex -mx-6">
+    <div class="w-1/2 px-6">
+      <div class="prose">
+        <p>
+          이러한 노력으로 삼표시멘트는 환경(Eco)과 에너지(Energy)를 결합한 ‘에코노지(Econergy)’를 구현함으로서 국내 시멘트 분야의 선두자로 자리매김하도록 최선의 노력을 다하겠습니다.
+        </p>
+      </div>
+    </div>
+    <div class="w-1/2 px-6">
+      <div class="prose">
+        <p>
+          연구 분야로는 시멘트 제조공정의 생산성 향상 및 에너지 절감 기술 개발과 산업부산물의 원재료 적용에 대한 공정 및 환경영향 검토를 하는 ‘공정연구 분야’와 다양한 고기능성 시멘트 및 원재료 개발 및 산업부산물을 활용한 자원순환형 혼합시멘트 개발, 친환경 에코시멘트 제조 기술개발을 하는 ‘시멘트 연구분야’가 있습니다.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
