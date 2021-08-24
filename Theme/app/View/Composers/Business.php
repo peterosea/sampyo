4fdeef68119621adbd86c7b32d40094e2d860dd5
999hyeon <999@hyeon.pro>
2021-08-24 15:04:12 +0900

    :construction: 비즈니스 - 컨텐츠 layout
    Resolves: #65

--- Theme/app/View/Composers/Business.php
+++ Theme/app/View/Composers/Business.php
@@ -25,6 +25,7 @@ class Business extends Composer
         return [
           'fixedMenu' => $this->fixedMenu(),
           'acf' => $this->get_business_info(),
+          'slug' => $this->get_slug(),
         ];
     }
 
@@ -66,5 +67,11 @@ class Business extends Composer
           'outlink' => $outlink,
           'area' => $area
         ];
+    }
+
+    public function get_slug()
+    {
+        $slug = get_post_field('post_name', get_the_ID());
+        return $slug;
     }
 }
--- Theme/resources/views/partials/content-single-business.blade.php
+++ Theme/resources/views/partials/content-single-business.blade.php
@@ -1,5 +1,5 @@
 @include('partials.header-single-business')
-<main class="page">
-
+<main class="business__page">
+  @include('partials.business.'. $slug)
 </main>
 @include('partials.footer-single-business')
