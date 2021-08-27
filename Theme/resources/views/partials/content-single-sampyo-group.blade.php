0dede365b5c76bddd01292a45be1fa5e5f70701c
999hyeon <999@hyeon.pro>
2021-08-27 18:12:58 +0900

    :construction: 삼표그룹 - CEO 메시지
    Resolves: beeclover/sampyo#74

--- Theme/resources/views/partials/content-single-sampyo-group.blade.php
+++ Theme/resources/views/partials/content-single-sampyo-group.blade.php
@@ -1,4 +1,4 @@
 @include('partials.header-single-sampyoGroup')
-<main class="sampyoGroup__page">
-  {{-- @include('partials.sampyo-group.'. $slug) --}}
+<main class="sampyoGroup__page business__page">
+  @include('partials.sampyo-group.'. $slug)
 </main>
---
+++ Theme/resources/views/partials/sampyo-group/chairman-message.blade.php
@@ -0,0 +1,53 @@
+<div class="container lg:max-w-screen-lg mx-auto">
+  <div class="business__page-title mb-20">
+    고객과 사회의 어려운 과제 해결에 힘이 되는 기업이 되겠습니다. 
+  </div>
+  <div class="grid grid-cols-2 gap-x-12 gap-y-28 mb-20">
+    <div class="prose">
+      <p class="mb-10">
+        삼표그룹은 1966년 ‘개척 정신’을 바탕으로 우리나라 기간 산업의 한 축으로서 지난 반세기 이상 지속적인 변화와 도전을 통해 고객과 사회 및 국가 발전에 이바지하며 성장을 이어왔습니다.  
+      </p>
+      <p class="mb-10">
+        급속한 경제 발전에 따른 대규모의 고품질 주택과 건설 수요에 대처하기 위해 끊임없는 기술 혁신과 고품질의 제품을 안정적으로 공급하며 고객 및 파트너와 함께 한국 사회의 인프라 건설 과제를 해결하는데 전력을 다했습니다.
+      </p>
+      <p class="mb-10">
+        인프라 건설의 기초가 되는 레미콘, 골재를 필두로 콘크리트와 시멘트 분야로 사업 영역을 확대하며 건설 소재 사업의 수직 계열화를 완성함으로써 국내 유일의 건설 소재 종합 솔루션 공급자(Total Solution Provider)로 자리매김했습니다. 나아가 철도 궤도 및 물류 분야에서도 탄탄한 입지를 구축했습니다.   
+      </p>
+    </div>
+    <div>
+      <img
+        src="@php echo get_theme_file_uri('resources/images/chairman_message-1.jpg') @endphp"
+        srcset="
+          @php echo get_theme_file_uri('resources/images/chairman_message-1@2x.jpg') @endphp 2x,
+          @php echo get_theme_file_uri('resources/images/chairman_message-1@3x.jpg') @endphp 3x"
+        alt=""
+      >
+    </div>
+    <div>
+      <img
+        src="@php echo get_theme_file_uri('resources/images/chairman_message-2.jpg') @endphp"
+        srcset="
+          @php echo get_theme_file_uri('resources/images/chairman_message-2@2x.jpg') @endphp 2x,
+          @php echo get_theme_file_uri('resources/images/chairman_message-2@3x.jpg') @endphp 3x"
+        alt=""
+      >
+    </div>
+    <div class="prose">
+      <p class="mb-10">
+        세상은 새로운 도전 과제에 직면했습니다. 기후 위기로 대변되는 지구온난화를 해결하기 위해 이산화탄소를 줄이고 폐자원의 활용과 관리 등 순환 자원의 가치를 높여야 합니다. 
+      </p>
+      <p class="mb-10">
+        삼표그룹은 한국 사회의 지속가능한 발전을 위해 건설 소재 및 인프라 사업에서 탄소 감축을 이루고 순환 자원 분야에서 사업을 다각화하고 있습니다. 영위하는 모든 사업에서 기술과 혁신을 통해 사회 인프라 구축에 필요한 다양한 요구를 충족시키며 새로운 도전과졔 해결에 힘이 되는 기업이 되겠습니다.  
+      </p>
+    </div>
+  </div>
+  <p class="text-2xl mb-11" style="line-height: 1.67">
+    <span class="pointUnderLine" style="display: inline; --bs: var(--RGB-atmosphere); --h: -0.6em">
+      협력사와 지역사회는 물론 모든 이해관계자와 상생하고 개척과 도전의 정신으로 고객의 성공과 한국사회가 당면한 과제 해결에 기여하고자 합니다.  
+    </span><br/>
+    <span>
+      감사합니다.
+    </span>
+  </p>
+  <p class="text-lg font-bold">삼표그룹 회장 | 정도원 </p>
+</div>
