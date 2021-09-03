13a91c1366dc03db1a098bd23f5b3b09a3917855
999hyeon <999@hyeon.pro>
2021-09-03 16:19:12 +0900

    :sparkles: 인재재채용 - 평가 및 보상
    Resolves: beeclover/sampyo#85

---
+++ Theme/resources/views/partials/career/evaluation.blade.php
@@ -0,0 +1,180 @@
+<main class="career__page business__page">
+<div class="container lg:max-w-screen-lg mx-auto">
+  <div class="business__page-title mb-8">
+    평가 및 보상
+  </div>
+  <div class="flex -mx-6">
+    <div class="w-1/2 px-6 prose">
+      <p>
+        삼표그룹은 채용부터 교육, 평가, 보상에 이르기까지 미래지향적 가치관을 가지고 끊임없는 자기계발로 가치 있는 성과를 만드는 도전하는 인재중심의 성과주의 인사제도를 운영하고 있습니다. 
+      </p>
+    </div>
+    <div class="w-1/2 px-6 prose">
+      <p>
+        목표수립을 시작으로 연간 운영되는 성과관리 프로세스를 통해 조직/개인의 성과목표를 체계적으로 관리하고, 이를 바탕으로 성과, 역량 그리고 잠재성을 종합적으로 평가합니다. 평가 결과에 따라 매년 연봉인상 조건이 차별적으로 적용되며 조직/개인의 성과는 공정하고 명확한 평가프로세스에 따라 적극적으로 보상합니다.
+      </p>
+    </div>
+  </div>
+  <div class="mb-20"></div>
+  <div class="font-black underline power text-2xl mb-5">
+    성과관리 프로세스
+  </div>
+  <div class="mb-16"></div>
+  <ul class="flex gap-x-2.5">
+    <li class="flex-1">
+      <div class="card__house bg-mistyLawn right mb-10">
+        <div class="card__house-inner h-24 flex flex-col items-center justify-center p-8">
+          <div class="text-xl font-normal">
+            목표수립
+          </div>
+        </div>
+      </div>
+      <div class="prose mr-8">
+        <p>
+          사업부문 별 KPI Pool 토대로 조직/개인의 목표수립, 임원/팀장/팀원 상호 협의
+        </p>
+      </div>
+    </li>
+    <li class="flex-1">
+      <div class="card__house bg-hospitalGreen right text-white mb-10">
+        <div class="card__house-inner h-24 flex flex-col items-center justify-center p-8">
+          <div class="text-xl font-bold">
+            중간점검/코칭
+          </div>
+        </div>
+      </div>
+      <div class="prose mr-8">
+        <p>
+          중간면담 실시 - 계획 실천 및 목표대비 실적 확인 / 목표달성을 위한 장애요인 파악 및 지도/조언
+        </p>
+      </div>
+    </li>
+    <li class="flex-1">
+      <div class="card__house bg-bermudagrass right text-white mb-10">
+        <div class="card__house-inner h-24 flex flex-col items-center justify-center p-8">
+          <div class="text-xl font-bold">
+            평가
+          </div>
+        </div>
+      </div>
+      <div class="prose mr-8">
+        <p>연초 목표/KPI 대비 달성도에 대한 평가 / 본인평가, 평가면담, 상사평가 / Calibration 및 확정</p>
+      </div>
+    </li>
+    <li class="flex-1">
+      <div class="flex flex-col items-center justify-center p-8 bg-bermudagrass h-24 text-white mb-10">
+        <div class="text-xl font-bold">
+          Feedback
+        </div>
+      </div>
+      <div class="prose">
+        <p>평가 결과에 대한 피드백 / 필요 시, 이의 신청 / 프로세스 미흡 성과 및 취약 역량에 대한 면담/코칭</p>
+      </div>
+    </li>
+  </ul>
+  <div class="mb-24"></div>
+  <div class="font-black underline power text-2xl mb-16">
+    성과관리 프로세스
+  </div>
+  <div class="flex mb-14">
+    <div class="w-1/2 relative">
+      <img
+        src="{{ get_theme_file_uri('resources/images/career-evaluation-0.png') }}"
+        srcset="
+          {{ get_theme_file_uri('resources/images/career-evaluation-0@2x.png') }} 2x,
+          {{ get_theme_file_uri('resources/images/career-evaluation-0@3x.png') }} 3x"
+        alt=""
+      >
+      <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white prose text-center w-full px-4">
+        <h3 class="text-lg">업적평가</h3>
+        <p>
+          경영목표 달성을 위한 개인 및 조직별 성과의 역량평가
+        </p>
+      </div>
+    </div>
+    <div class="w-1/2">
+      <img
+        src="{{ get_theme_file_uri('resources/images/career-evaluation-1.png') }}"
+        srcset="
+          {{ get_theme_file_uri('resources/images/career-evaluation-1@2x.png') }} 2x,
+          {{ get_theme_file_uri('resources/images/career-evaluation-1@3x.png') }} 3x"
+        alt=""
+      >
+      <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white prose text-center w-full px-4">
+        <h3 class="text-lg">역량평가</h3>
+        <p>
+          직무, 직급에 따른 보유 역량 평가
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="border-t-2 border-b-2 border-blackout">
+    <div class="flex -mx-9 py-12">
+      <div class="w-1/3 px-9">
+        <div class="card__s1 gap-9">
+          <div class="card__s1-thumbnail_ flex items-center justify-center">
+              <img
+              src="{{ get_theme_file_uri('resources/images/career-evaluation-2.png') }}"
+              srcset="
+                {{ get_theme_file_uri('resources/images/career-evaluation-2@2x.png') }} 2x,
+                {{ get_theme_file_uri('resources/images/career-evaluation-2@3x.png') }} 3x"
+              alt=""
+            >
+          </div>
+          <div class="card__s1-body items-center justify-center text-center gap-5">
+            <div class="card__s1-title text-xl font-bold">
+              보상
+            </div>
+            <div class="card__s1-content">
+              연봉, 성과급
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-1/3 px-9">
+        <div class="card__s1 gap-9">
+          <div class="card__s1-thumbnail_ flex items-center justify-center">
+              <img
+              src="{{ get_theme_file_uri('resources/images/career-evaluation-3.png') }}"
+              srcset="
+                {{ get_theme_file_uri('resources/images/career-evaluation-3@2x.png') }} 2x,
+                {{ get_theme_file_uri('resources/images/career-evaluation-3@3x.png') }} 3x"
+              alt=""
+            >
+          </div>
+          <div class="card__s1-body items-center justify-center text-center gap-5">
+            <div class="card__s1-title text-xl font-bold">
+              교육, 직무이동
+            </div>
+            <div class="card__s1-content">
+              육성척도
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-1/3 px-9">
+        <div class="card__s1 gap-9">
+          <div class="card__s1-thumbnail_ flex items-center justify-center">
+              <img
+              src="{{ get_theme_file_uri('resources/images/career-evaluation-4.png') }}"
+              srcset="
+                {{ get_theme_file_uri('resources/images/career-evaluation-4@2x.png') }} 2x,
+                {{ get_theme_file_uri('resources/images/career-evaluation-4@3x.png') }} 3x"
+              alt=""
+            >
+          </div>
+          <div class="card__s1-body items-center justify-center text-center gap-5">
+            <div class="card__s1-title text-xl font-bold">
+              승진
+            </div>
+            <div class="card__s1-content">
+              심사기준
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="mb-24"></div>
+</div>
+</main>
