cb29278efe1e3a4f85d82e72df07f65ccca5947a
999hyeon <999@hyeon.pro>
2021-09-08 15:07:55 +0900

    :sparkles: 비즈니스 - 프리캐스트 콘크리트
    Resolves: beeclover/sampyo#92

---
+++ Theme/resources/views/partials/business/precast-concrete.blade.php
@@ -0,0 +1,103 @@
+<main class="business__page">
+<div class="container lg:max-w-screen-lg mx-auto">
+  <section class="mb-32">
+    <div class="business__page-title mb-11">
+      국내 프리캐스트 콘크리트 산업의<br/>
+      미래를 선도해 나가겠습니다.
+    </div>
+    <div class="prose mb-24 pb-1">
+      <p>
+        삼표는 프리캐스트 콘크리트(Precast Concrete) 분야에서 국내 최고 수준의 생산 설비와 우수한 기술력을 바탕으로 국내 건설산업의 새로운 영역을 개척해 왔습니다. 우리는 현재에 안주하지 않고 미래를 향한 끊임없는 기술개발과 생산시스템의 패러다임 혁신을 통해 고객 만족 실현과 국내 프리캐스트 콘크리트 산업의 미래를 선도해 나갈 것을 약속합니다.
+      </p>
+    </div>
+    <div class="coverbox-full-fixedHeight right pt-28" style="--h: 400px">
+      <div class="pointUnderLine text-2xl font-bold mb-6" style="--h: -0.5em">
+        프리캐스트 콘크리트(Precast Concrete)란?
+      </div>
+      <div class="prose mb-16 pb-1.5">
+        <p>
+          프리캐스트 콘크리트는 건설현장에서 사용되는 콘크리트 구조체(기둥, 보, 슬라브, 벽체 등)를 공장에서 엄격한 공정관리 하에 사전에 제작/생산하고, 이를 건설현장으로 운반하여 조립/시공하는 건설방식입니다. 공기단축, 고품질, 경제성, 친환경 등의 장점으로 지속성장하고 있는 분야입니다.
+        </p>
+      </div>
+      <div class="flex mb-5">
+        <div class="w-1/2">
+          @include('partials.img', ['ex' => 'jpg', 'name' => 'business-precast-0'])
+        </div>
+        <div class="w-1/2">
+          @include('partials.img', ['ex' => 'jpg', 'name' => 'business-precast-1'])
+        </div>
+      </div>
+      @include('partials.img', ['ex' => 'jpg', 'name' => 'business-precast-2'])
+    </div>
+  </section>
+  <section class="mb-20">
+    <div class="business__page-title mb-10">
+      삼표가 만들면 다릅니다.
+    </div>
+    <div class="prose mb-28 pb-2">
+      <p>
+        설계/생산/시공/유지관리에 이르는 토탈 서비스(Total Service)를 제공하여 고객과의 신뢰를 약속합니다.
+      </p>
+    </div>
+    <div class="letter__more text-2xl font-black text-center mb-20">
+      <span class="inline-block bg-white px-8 relative">
+        Total Engineering Service 제공
+      </span>
+    </div>
+    <div class="flex -mx-7">
+      <div class="w-1/4 px-7">
+        <div class="card__s1">
+          <div class="card__s1-thumbnail_">
+              @include('partials.img', ['ex' => 'svg', 'name' => 'business-precast-3'])
+          </div>
+          <div class="card__s1-body items-start text-left gap-2.5">
+            <div class="card__s1-title text-xl font-bold">설계</div>
+            <div class="card__s1-content">
+              자체적인 PC설계 조직의 보유로 제품에 대한 고객 Needs 반영과 우수한 품질 확보. 국내 최상위 수준의 기술연구소를 보유하여 다양하고 독자적인 공법 및 제품 보유
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-1/4 px-7">
+        <div class="card__s1">
+          <div class="card__s1-thumbnail_">
+              @include('partials.img', ['ex' => 'svg', 'name' => 'business-precast-4'])
+          </div>
+          <div class="card__s1-body items-start text-left gap-2.5">
+            <div class="card__s1-title text-xl font-bold">생산</div>
+            <div class="card__s1-content">
+              업계 최대 생산능력을 기반으로 안정적인 제품 생산을 통한 납품 일정 준수
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-1/4 px-7">
+        <div class="card__s1">
+          <div class="card__s1-thumbnail_">
+              @include('partials.img', ['ex' => 'svg', 'name' => 'business-precast-5'])
+          </div>
+          <div class="card__s1-body items-start text-left gap-2.5">
+            <div class="card__s1-title text-xl font-bold">시공</div>
+            <div class="card__s1-content">
+              풍부한 PC시공 경험을 통한 Know-how 보유로 공기 단축
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-1/4 px-7">
+        <div class="card__s1">
+          <div class="card__s1-thumbnail_">
+              @include('partials.img', ['ex' => 'svg', 'name' => 'business-precast-6'])
+          </div>
+          <div class="card__s1-body items-start text-left gap-2.5">
+            <div class="card__s1-title text-xl font-bold">유지관리</div>
+            <div class="card__s1-content">
+              시공 후 하자, 민원 등 고객의 목소리에 즉시 대응할 수 있는 CS전담 조직 확보
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+</main>
--- Theme/resources/views/partials/img.blade.php
+++ Theme/resources/views/partials/img.blade.php
@@ -3,8 +3,10 @@
       class="{{ $class }}"
   @endif
   src="@php echo get_theme_file_uri('resources/images/'.$name.'.'.$ex) @endphp"
+  @if ($ex !== 'svg')
   srcset="
     @php echo get_theme_file_uri('resources/images/'.$name.'@2x.'.$ex) @endphp 2x,
     @php echo get_theme_file_uri('resources/images/'.$name.'@3x.'.$ex) @endphp 3x"
+  @endif
   alt=""
 >
