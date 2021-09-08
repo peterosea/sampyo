c9008b7f4a5400b0af0f8c8851817039280588c2
999hyeon <999@hyeon.pro>
2021-09-08 17:31:26 +0900

    :sparkles: 비즈니스 - 철도
    Resolves: beeclover/sampyo#94

---
+++ Theme/resources/views/partials/business/railway.blade.php
@@ -0,0 +1,150 @@
+<main class="business__page">
+<div class="container lg:max-w-screen-lg mx-auto">
+  <section class="mb-32">
+    <div class="business__page-title mb-10">
+      No.1 철도궤도 종합엔지니어링 기업
+    </div>
+    <div class="grid grid-cols-2 gap-x-12 gap-y-16">
+      <div class="prose">
+        <p>
+          1980년 강원산업(주) 철도사업부로 시작한 삼표레일웨이는 대한민국 철도산업의 발전과 그 역사를 함께 해 왔습니다.<br/>
+          1997년 국내 최초로 분기기(Turnout) 생산 공장을 설립하여 철도궤도분야의 핵심제품인 분기기를 비롯한 각종 궤도용품의 양산 체계를 갖추었으며, 적극적인 기술개발 및 과감한 투자를 통해 세계적 수준의 기술력을 확보하였습니다. 지속적인 노력을 통해 얻은 글로벌 경쟁력을 바탕으로 삼표레일웨이는 국내를 넘어 해외시장에도 적극적으로 진출하여 고객으로부터 기술력과 품질에 대한 호평을 받고 있습니다.
+        </p>
+      </div>
+      <div class="prose">
+        <p>
+          분기기 등 철도궤도용품 제작에서 시작한 삼표레일웨이는 현재 철도궤도시공, PST(Precast Slab Track) 패널 제작 및 시공, 레일연마, 시설물 유지보수사업 등 철도궤도부문 전 분야를 아우르는 종합 철도 엔지니어링 기업으로 
+          성장하였습니다.<br/>
+          앞으로도 지금까지의 성공에 안주하지 않고, 철저한 품질관리/차별화된 
+          고객 서비스/과감한 기술투자 그리고 적극적인 시장 개척을 통해 글로벌 기업으로 성장하겠습니다.
+        </p>
+      </div>
+      <div class="coverbox-label mb-12">
+        @include('partials.img', ['ex' => 'jpg', 'name' => 'business-railway-0'])
+        <span class="label text-white">
+          국내 최초 ‘고속철도 분기기’ 개발 및 세계일류상품 선정
+        </span>
+      </div>
+      <div class="coverbox-label mb-12">
+        @include('partials.img', ['ex' => 'jpg', 'name' => 'business-railway-1'])
+        <span class="label text-white">
+          국내 최초 개발<br/>
+          사전제작형 콘크리트 슬리브 궤도(PSTS)공법
+        </span>
+      </div>
+    </div>
+  </section>
+  <section class="mb-32">
+    <div class="coverbox-full-fixedHeight left pt-24 mb-24" style="--h: 669px">
+      <div class="business__page-title mb-12">
+        최고의 철도궤도 제품 및 서비스
+      </div>
+      <h3 class="underline power font-black text-2xl mb-11">철도궤도용품(분기기)</h3>
+      <div class="grid grid-cols-2 gap-x-12 mb-16 pb-1.5">
+        <div class="prose">
+          <p>
+            분기기란 선로를 주행중인 열차의 진로를 다른 선로로 전환시키는 장치로써, 선로에서 가장 중요한 궤도 및 신호부문의 종합 구조물입니다.<br/>
+            삼표레일웨이는 최고의 기술력을 바탕으로 분기기를 비롯한 국내 철도궤도용품 시장을 선도하고 있습니다. 경부고속철도용 분기기를 시작으로, 국내 최초로 콘크리트궤도용 고속분기기를 국산화하여 호남고속철도 사업에 적용하는 등 국내 분기기 기술 향상과 철도 안전에 기여하였으며, 철도궤도용품 산업의 경쟁력을 향상시켰습니다. 
+          </p>
+        </div>
+        <div class="prose">
+          <p>
+            이러한 경쟁력은 해외 시장 진출에도 이어져, 대만, 파키스탄, 이란, 필리핀, 베트남 등으로 수출을 꾸준히 확대하고 있습니다. 특히 2021년에는 대만 철도 개량사업의 분기기 1,600틀 공급계약을 수주하여 해외 수출 규모를 비약적으로 확대하는 등 해외 수출을 통해 국내 철도궤도용품의 우수성을 세계로 전파하고 있습니다.
+          </p>
+        </div>
+      </div>
+      @include('partials.img', ['ex' => 'jpg', 'name' => 'business-railway-2'])
+    </div>
+    <h3 class="underline power font-black text-2xl mb-8">
+      사전제작형 콘크리트 슬래브 궤도(PSTS)
+    </h3>
+    <div class="grid grid-cols-2 gap-x-12 gap-y-16 mb-16">
+      <div class="prose">
+        <p>
+          삼표레일웨이는 10여년간의 노력과 투자로 콘크리트 슬래브 패널을 공장에서 사전제작하여 현장에서 시공하는 조립식 콘크리트 궤도 구조인 PSTS(Precast Slab Track System)는를 상용화했습니다. 이는 현장상황에 따라 균일한 품질확보가 어려운 현장타설 콘크리트 궤도의 단점을 극복한 것으로 국내 궤도시공 기술의 패러다임을 바꾸었습니다.
+        </p>
+      </div>
+      <div class="prose">
+        <p>
+          친환경성, 시공성, 경제성, 품질 및 유지보수 등 모든 면에서 국내 철도궤도산업의 수준을 한단계 발전시키는데 기여하고 있습니다.<br/>
+          2013년 국내 최초의 PST 시스템 전용 생산라인이 신설하여 표준화된 고품질 제품을 생산하고 있으며, 원주-제천, 진주-광양, 신경주-포항 등 국가철도망<br/>
+          구축의 파트너로서 활약하고 있습니다
+        </p>
+      </div>
+      <div class="coverbox-label">
+        @include('partials.img', ['ex' => 'jpg', 'name' => 'business-railway-3'])
+        <span class="label text-white">
+          사전 제작형 콘크리트 슬리브 궤도(PSTS)
+        </span>
+      </div>
+      @include('partials.img', ['ex' => 'jpg', 'name' => 'business-railway-4'])
+    </div>
+    <h3 class="underline power font-black text-2xl mb-8">
+      레인연마/유지보수
+    </h3>
+    <div class="grid grid-cols-2 gap-x-12 gap-y-16 mb-16">
+      <div class="prose">
+        <p>
+          레일연마란 선로를 구성하는 레일의 품질을 지속적으로 유지시키기 위해 거칠어진 레일두부표면을 정밀하게 갈고 닦아내는 유지보수 기술의 한 분야입니다.
+        </p>
+      </div>
+      <div class="prose">
+        <p>
+          삼표레일웨이는 30석 대형 레일연마차량을 2013년 도입하여 운행하였고, 2017년 및 2019년에 24석 대형 레일연마차량을 추가 도입함으로써 열차 운행의 쾌적함과 안전함을 높이는데 일조하고 있습니다.<br/>
+          뿐만 아니라, 철도시설물 유지보수 사업에도 참여하여 유지보수 분야의 새로운 기준을 만들어가기 위해 노력하고 있습니다.
+        </p>
+      </div>
+      <div class="col-span-2">
+        @include('partials.img', ['ex' => 'jpg', 'name' => 'business-railway-5'])
+      </div>
+    </div>
+    <h3 class="underline power font-black text-2xl mb-8">
+      철도 궤도 시공
+    </h3>
+    <div class="grid grid-cols-2 gap-x-12 gap-y-16 mb-16">
+      <div class="prose">
+        <p>
+          철도궤도시공은 열차의 안전한 운행과 쾌적한 승차감에 직접적인 영향을 
+          주는 중요한 공정으로, 레일/침목/체결장치 등을 조합하여 열차가 주행하는 
+          구조물을 시공하는 사업입니다. 
+        </p>
+      </div>
+      <div class="prose">
+        <p>
+          삼표레일웨이는 경부고속철도, 호남고속철도 등 국내 대형 고속철도사업 
+          사업 뿐만 아니라, 일반철도, 경전철 등 각종 궤도시공사업을 성공리에 
+          시공하여 국내 최고의 궤도시공 능력을 인정받고 있습니다
+        </p>
+      </div>
+      <div class="coverbox-label">
+        @include('partials.img', ['ex' => 'jpg', 'name' => 'business-railway-6'])
+        <span class="label text-white">
+          플래시벗 용접장비
+        </span>
+      </div>
+      <div class="coverbox-label">
+        @include('partials.img', ['ex' => 'jpg', 'name' => 'business-railway-7'])
+        <span class="label text-white">
+          30석 초대형 레일 연마 장비
+        </span>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="business__page-title mb-10">
+      기술연구소
+    </div>
+    <div class="grid grid-cols-2 gap-x-12">
+      <div class="prose">
+        <p>
+          철도기술연구소는 국내철도산업 발전을 위해 2001년 설립되어 분기기 등 궤도분야 전문가로 구성된 연구원들이 철도궤도용품의 개발과 안전성 향상을 위해 다양한 연구활동을 수행하고 있습니다. 세계 4번째 고속철도 운영국에 걸맞는 궤도인프라 기술개발로 고속분기기 국산화에 성공하였으며, 선진국 수준의 궤도기술 발전을 위해 노력하고 있습니다.
+        </p>
+        <p>
+          삼표레일웨이는 국토교통과학기술진흥원, 한국산업기술진흥협회로부터 분기기, PSTS 등의 신기술을 인증받았을 뿐 아니라, 철도관련 다양한 연구개발과제 수행을 통해 국내 철도기술혁신의 일익을 담당하고 있습니다.
+        </p>
+      </div>
+      @include('partials.img', ['ex' => 'jpg', 'name' => 'business-railway-8'])
+    </div>
+  </section>
+</div>
+</main>
