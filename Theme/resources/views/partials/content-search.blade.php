5b0ce5cc5bb2e63beed806447e748eef0291c5f5
999hyeon <999@hyeon.pro>
2021-09-09 23:45:58 +0900

    :sparkles: 검색 (beeclover/sampyo#113)

--- Theme/resources/views/partials/content-search.blade.php
+++ Theme/resources/views/partials/content-search.blade.php
@@ -1,15 +1,17 @@
-<article @php(post_class())>
-  <header>
-    <h2 class="entry-title">
-      <a href="{{ get_permalink() }}">
-        {!! $title !!}
-      </a>
-    </h2>
-
-    @includeWhen(get_post_type() === 'post', 'partials/entry-meta')
-  </header>
-
-  <div class="entry-summary">
-    @php(the_excerpt())
+<div class="py-10 border-b border-superSilver">
+  <div class="card-def">
+    @if ($thumbnail)
+      <div class="card-def-thumbnail">
+        {!! $thumbnail !!}
+      </div>
+    @endif
+    <div class="card-def-main">
+      <div class="card-def-contentWrap">
+        <a href="{!! $permalink !!}" class="card-def-title text-lg">{!! $title !!}</a>
+        <a href="{!! $permalink !!}" class="card-def-content mb-0">
+          {!! $excerpt !!}
+        </a>
+      </div>
+    </div>
   </div>
-</article>
+</div>
