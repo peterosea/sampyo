bc6cca99a99003dd58ed559a750b38e49671b8f7
999hyeon <999@hyeon.pro>
2021-10-03 18:10:54 +0900

    :sparkles: mobile/ 비즈니스 - 골재
    Resolves: beeclover/sampyo#162

--- Theme/resources/views/partials/business/aggregate.blade.php
+++ Theme/resources/views/partials/business/aggregate.blade.php
@@ -4,24 +4,24 @@
     국내 최초 KS인증 골재로 국내 최대 생산량과<br class="hidden sm:block" />
     최고의 품질을 자랑합니다.
   </div>
-  <div class="grid grid-cols-2 gap-x-12 gap-y-20">
-    <div>
+  <div class="grid grid-cols-1 sm:grid-cols-2 gap-x-6 sm:gap-x-12 gap-y-px-30 sm:gap-y-20">
+    <div class="max:sm:order-2">
       <p>
         1974년 하천골재 사업을 시작으로 1986년과 1988년에 화성과 양주에 각각 대규모의 석산 사업장을 개설하는 등 총 5개의 석산골재와 1개의 바다골재, 4개의 선별파쇄 산지를 운영하며 골재의 원활한 유통과 안정적인 품질을 통해 수도권 골재 시장을 선도하고 있습니다. 
       </p>
     </div>
-    <div>
+    <div class="max:sm:order-4">
       <p>
         또한, 지속적인 연구/개발을 통해 고품질의 골재를 생산하기 위해 노력하고 있으며, 2006년에는 골재업계 최초로 석산 KS인증을 취득함으로써 국내 골재산업의 고품질화, 나아가서는 콘크리트의 고품질화를 선도하고 있습니다. 앞으로도 고품질 골재 생산 노하우를 바탕으로 우수한 품질과 균일한 규격의 골재를 전국 곳곳에 안정적이고 지속적으로 공급해 나갈 것입니다.
       </p>
     </div>
-    <div>
+    <div class="max:sm:order-1">
       <div class="coverbox-label">
         @include('partials.img', ['ex' => 'jpg', 'name' => '11'])
         <span class="label text-white">국토부 주관 골재채취능력평가 13년 연속 1위</span>
       </div>
     </div>
-    <div>
+    <div class="max:sm:order-3">
       <div class="coverbox-label">
         @include('partials.img', ['ex' => 'jpg', 'name' => 'invalid-name'])
         <span class="label text-white">국토부 주관 골재채취능력평가 13년 연속 1위</span>
@@ -29,7 +29,7 @@
     </div>
   </div>
   <div class="mb-px-70 sm:mb-32"></div>
-  <div class="coverbox-full-fixedHeight left pt-24">
+  <div class="coverbox-full-fixedHeight left max:sm:py-px-30 sm:pt-24">
     <div class="page__level-title mb-4">
       안정적인 골재 공급 능력 보유
     </div>
@@ -42,24 +42,24 @@
   <div class="page__level-title mb-px-30 sm:mb-11">
     골재채취능력평가 시작 이래 13년 연속 1위
   </div>
-  <div class="grid grid-cols-2 gap-x-12 gap-y-20">
-    <div>
+  <div class="grid grid-cols-1 sm:grid-cols-2 gap-x-12 gap-y-px-30 sm:gap-y-20">
+    <div class="max:sm:order-1">
       <p>
         국토교통부에서 2008년부터 시작한 골재채취능력평가에서 13년째 1위를 놓치지 않고 있습니다. 골재채취능력평가는 골재채취실적과 보유장비, 신인도 등을 종합적으로 평가하는 것으로, 이는 가장 신뢰할 수 있는 골재 공급업체라는 것을 인정받았다고 할 수 있습니다.
       </p>
     </div>
-    <div>
+    <div class="max:sm:order-3">
       <p>
         당사는 10개의 골재산지에서 연간 1,300만㎥ 이상의 레미콘용 골재를 공급할 수 있는 능력을 갖추고 있으며, 이는 수도권 레미콘용 골재 공급량의 15%(총 8,200만㎥)에 육박하는 물량입니다. 또한, 아스콘용, 토목공사용 골재 등 다양한 제품을 생산하고 있습니다.
       </p>
     </div>
-    <div>
+    <div class="max:sm:order-2">
       <div class="coverbox-label">
         @include('partials.img', ['ex' => 'jpg', 'name' => '433'])
         <span class="label text-white">석산 양석 채취</span>
       </div>
     </div>
-    <div>
+    <div class="max:sm:order-4">
       <div class="coverbox-label">
         @include('partials.img', ['ex' => 'jpg', 'name' => '434'])
         <span class="label text-white">바다모래 채취</span>
@@ -70,13 +70,13 @@
   <div class="page__level-title mb-px-30 sm:mb-11">
     국내 최초 KS인증의 우수하고 균일한 품질의 골재
   </div>
-  <div class="flex -mx-6 mb-20">
-    <div class="w-1/2 px-6">
+  <div class="grid grid-cols-1 sm:grid-cols-2 gap-x-12 mb-px-30 sm:mb-20">
+    <div>
       <p>
         골재는 콘크리트, 아스팔트 등 에 혼합 사용되는 매우 중요한 건설 자재로 건축물 수명에 큰 영향을 미치고 있어 우수하고 균일한 품질이 필요합니다.
       </p>
     </div>
-    <div class="w-1/2 px-6">
+    <div>
       <p>
         당사는 철저한 품질검사와 품질관리를 통해 국내 최초로 KS인증을 취득한 것을 시작으로 골재부문 6개 석산 모두가 KS인증을 취득해 골재품질의 표준화를 이끌고 있으며, 높은 압축강도를 지닌 최상의 원석으로 우수한 품질과 균일한 사이즈의 골재를 공급하고 있습니다
       </p>
@@ -87,15 +87,15 @@
   <div class="page__level-title mb-px-30 sm:mb-11">
     환경친화적인 자원 개발
   </div>
-  <div class="flex -mx-6">
-    <div class="w-1/2 px-6">
+  <div class="grid grid-cols-1 sm:grid-cols-2 gap-x-12 gap-y-px-30">
+    <div>
       <p>
         1970년대 이후 대한민국 경제발전에 따른 수도권 도시개발은 건설기초소재인 골재의 적절한 공급이 있었기에 가능했습니다.<br/>
         제한된 자원인 골재의 완전한 활용과 개발 이후의 복구까지 환경친화적인 산업이 될 수 있도록 환경평가, 경제성 평가 등 철저한 사전계획 하에 골재채취 사업을 시행하고 있습니다.
       </p>
     </div>
-    <div class="w-1/2 px-6">
-      @include('partials.img', ['ex' => 'jpg', 'name' => '156', 'class' => 'transform -translate-x-10'])
+    <div class="max:sm:order-first">
+      @include('partials.img', ['ex' => 'jpg', 'name' => '156', 'class' => 'sm:transform sm:-translate-x-10'])
     </div>
   </div>
 </div>
