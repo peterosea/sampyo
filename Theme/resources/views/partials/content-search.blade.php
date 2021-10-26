9ca0f1f5251745b6e601bfbec2810b98abfbf281
999hyeon <999@hyeon.pro>
2021-10-26 12:59:04 +0900

    :sparkles: 검색 플러그인활용 함수 사용

--- Theme/resources/views/partials/content-search.blade.php
+++ Theme/resources/views/partials/content-search.blade.php
@@ -2,7 +2,7 @@
   @include('partials.card.__def', [
     'thumbnail' => $thumbnail,
     'permalink' => $permalink,
-    'title' => $title,
+    'title' => relevanssi_the_title(),
     'excerpt' => $excerpt,
   ])
 </div>
