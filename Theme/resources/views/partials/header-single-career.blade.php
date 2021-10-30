d2073c3676afecf1b63f9bf9c99ed2126c672588
999hyeon <999@hyeon.pro>
2021-10-30 20:12:20 +0900

    :lipstick: mobile fixed menu 짤림 수정

--- Theme/resources/views/partials/header-single-career.blade.php
+++ Theme/resources/views/partials/header-single-career.blade.php
@@ -1,4 +1,4 @@
-<div class="overflow-x-hidden header-archive-root2">
+<div class="xl:overflow-x-hidden header-archive-root2">
   <div class="overflow-hidden header-archive-root">
     <header class="header-archive">
       <div class="container xl:max-w-screen-xl mx-auto max:labtop:px-0">
