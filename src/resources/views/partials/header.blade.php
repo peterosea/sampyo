36153e937363efcd9644c4ca178759eba5e41cbd
Seunghan-Lee <jushan1980@gmail.com>
2022-02-10 22:31:46 +0900

    언어메뉴 업데이트 0210

--- src/resources/views/partials/header.blade.php
+++ src/resources/views/partials/header.blade.php
@@ -29,6 +29,7 @@
       </div>
     @endif
     <div class="header-global-search flex items-center">
+      <div id="weglot_here" class="lang_selector only_admin"></div>
       <button x-on:click.prevent="pop_search = true" class="focus:outline-none">
           <svg viewBox="0 0 56.966 56.966" class="w-[25px] m_sm:w-[18px]">
             <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23
