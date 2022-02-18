c07ff606d3294313ccf5d9e4ddcd41b520e8c453
Seunghan-Lee <jushan1980@gmail.com>
2022-02-18 15:23:30 +0900

    블로그 픽스메뉴 URL 변경

--- src/resources/views/partials/nav-fixed.blade.php
+++ src/resources/views/partials/nav-fixed.blade.php
@@ -5,7 +5,7 @@
 >
   <ul class="items-fixed-wrap">
     <li class="items-fixed-item @if ($category_label === "blog_category") current @endif">
-      <a href="/blog">블로그</a>
+      <a href="{{url('/blog')}}">블로그</a>
     </li>
     <li class="items-fixed-item @if ($category_label === "media_category") current @endif">
       <a href="/media">미디어</a>
